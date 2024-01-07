8a3244c295d89de03920aa255e3946234d9b3656
yashdanej <yashdanej2004@gmail.com>
2024-01-07 15:31:06 +0530

    adding cors setting

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.unsubscribe(
 // app.use(passport.initialize());
 // app.use(passport.session());
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://blog-meta.vercel.app'],  // i am doing this because of cookie request
+    origin: ['https://blog-meta.vercel.app'],  // i am doing this because of cookie request
     credentials: true,
 }));
 // for google auth20 START
